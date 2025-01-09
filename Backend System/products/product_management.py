7cda04e642b07bbc18c854d2e9aa5944569ce664
Farhan Tahmid <farhantahmid881@gmail.com>
2025-01-10 02:25:50 +0600

    product management algorithm started

---
+++ Backend System/products/product_management.py
@@ -0,0 +1,21 @@
+from .models import *
+
+# Manage Product Types
+def create_product_type(type,description):
+    pass
+
+def update_product_type(type,description):
+    pass
+
+def delete_product_type(product_type_id):
+    pass
+
+# Manage Product Category
+def create_product_category(product_type_id,category,description):
+    pass
+
+def update_product_categor(product_type_id,category,description):
+    pass
+
+def delete_product_category(product_type_id):
+    pass
