944f2eeb173978584c7fe8530c1dc04eb48a8b74
Farhan Tahmid <farhantahmid881@gmail.com>
2025-01-11 15:30:53 +0600

    error log creation function created

--- Backend System/system/models.py
+++ Backend System/system/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class ErrorLogs(models.Model):
-    timestamp = models.DateTimeField(auto_now_add=True)  # Automatically logs the time of the error
-    error_type = models.CharField(max_length=255)  # The type of error, e.g., "DatabaseError"
-    error_message = models.TextField()  # The detailed error message
+    timestamp = models.DateTimeField(auto_now_add=True,null=True)  # Automatically logs the time of the error
+    error_type = models.CharField(max_length=255,null=True,blank=True)  # The type of error, e.g., "DatabaseError"
+    error_message = models.TextField(null=True,blank=True)  # The detailed error message
 
     class Meta:
         verbose_name = "Error Log"
