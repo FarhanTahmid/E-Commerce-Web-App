9e1659e7f83667d19f89d5381b7281ad2795a179
Sakib2186 <skmdsakib2186@gmail.com>
2025-01-19 23:16:41 +0600

    .

--- Backend System/server_api/views.py
+++ Backend System/server_api/views.py
@@ -30,44 +30,6 @@ class FetchProductCategoryView(APIView):
 
 class CreateProductCategoryView(APIView):
    
-    """
-    API view to create a new product category.
-
-    This view handles the creation of a new product category. It expects the request data to contain
-    the category name and description. If either of these fields is missing, it returns a 400 Bad Request response.
-    If the category is created successfully, it returns a 201 Created response with a success message.
-    If there is an error during the creation process, it returns a 400 Bad Request response with an error message.
-
-    Attributes:
-        serializer_class (Serializer): The serializer class used for validating and serializing the product category data.
-
-    Methods:
-        post(request, format=None):
-            Handles the POST request to create a new product category.
-
-    Example Usage:
-        POST /api/product-categories/
-        {
-            "category_name": "Skincare",
-            "description": "Products for skincare"
-        }
-
-        Response (201 Created):
-        {
-            "message": "Product category created successfully!"
-        }
-
-        Response (400 Bad Request):
-        {
-            "error": "Both 'name' and 'description' are required."
-        }
-
-        Response (400 Bad Request):
-        {
-            "error": "An unexpected error occurred while creating the product category. Please try again later."
-        }
-    """
-
     serializer_class = product_serializers.Product_Category_Serializer
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
