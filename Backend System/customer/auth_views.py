20d00f01c5316a8c447e2760914ea1d23a4ef92c
Farhan Tahmid <farhantahmid881@gmail.com>
2025-01-30 14:00:58 +0600

    cart views

--- Backend System/customer/auth_views.py
+++ Backend System/customer/auth_views.py
@@ -7,14 +7,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from system.models import Accounts
-from rest_framework_simplejwt.tokens import RefreshToken,AccessToken
+from rest_framework_simplejwt.tokens import RefreshToken
 from django_ratelimit.exceptions import Ratelimited
 from django.core.exceptions import PermissionDenied
 from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import AllowAny,IsAuthenticated
 from json.decoder import JSONDecodeError
-from datetime import timedelta
 
 class CustomerSignupView(APIView):
     """
