04460b4aa6022a920673db944a57f9d8d1a51e23
Sakib2186 <skmdsakib2186@gmail.com>
2025-02-11 21:12:06 +0600

    .

--- Backend System/orders/serializers.py
+++ Backend System/orders/serializers.py
@@ -4,10 +4,37 @@ from .models import (
     OrderDetails,
     OrderShippingAddress,
     OrderPayment,
+    Cart,
+    CartItems
 )
 
 from customer.models import Accounts,CustomerAddress,Coupon
 from django.utils import timezone
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product_details = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CartItems
+        fields = ['id', 'product_sku', 'quantity', 'product_details']
+        read_only_fields = ['id']
+
+    def get_product_details(self, obj):
+        return {
+            'name': obj.product_sku.product_id.product_name,
+            'price': str(obj.product_sku.product_price),
+            'sku': obj.product_sku.product_sku,
+            'color': obj.product_sku.product_color,
+            'size': obj.product_sku.product_size
+        }
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(source='cartitems_set', many=True, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'cart_total_amount', 'items']
+        read_only_fields = ['id', 'cart_total_amount', 'items']
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
